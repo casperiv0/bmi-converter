75762d9899c0f72c0c9beddd8586d2e6ff28d62f
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-05-15 09:34:41 +0200

    added demo to README.md

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
 # Convert your weight and height into BMI
 
+Simple React app to calculate your BMI.
+
+## Demo
+
+**Live Demo:** [Github Pages](https://dev-caspertheghost.github.io/bmi-converter/)
+
 ## What I learned
 
 - Convert BMI
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { InputLabel, FormControl, Input, Button, Typography } from "@material-ui/core"
+import { InputLabel, FormControl, Input, Button, Typography, Link } from "@material-ui/core"
 
 
 class App extends Component {
@@ -31,9 +31,9 @@ class App extends Component {
   getBmi = (e) => {
     e.preventDefault();
     const { weight, height } = this.state;
-    const bmi = (weight / ((height * height)  / 10000 )).toFixed(2)
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2)
 
-    
+
 
     if (height === 0) {
       return this.setState({
@@ -44,14 +44,14 @@ class App extends Component {
     this.setState({
       bmi: bmi
     })
-    
+
     if (bmi <= 18.5) {
       return this.setState({
         state: "Underweight",
         color: "orange"
       });
     };
-    
+
     if (bmi >= 18.5 && bmi <= 25) {
       return this.setState({
         state: "Normal",
@@ -60,7 +60,7 @@ class App extends Component {
     }
 
 
-    if(bmi >= 25 && bmi <= 30) {
+    if (bmi >= 25 && bmi <= 30) {
       return this.setState({
         state: "Overweight",
         color: "orange"
@@ -80,29 +80,30 @@ class App extends Component {
     const { weight, height, bmi, state, color } = this.state
     return (
       <div className="App" >
-        <form  onSubmit={this.getBmi}>
-        <FormControl fullWidth>
-          <InputLabel>Enter your Weight in kilograms</InputLabel>
-          <Input type="number" value={weight} onChange={this.handleWeight} />
-        </FormControl>
-        <div className="margin-top">
+        <Link target="_blank" variant="body2" href="https://github.com/Dev-CasperTheGhost/bmi-converter" >Source Code</Link>
+        <form  style={{marginTop: "20px"}} onSubmit={this.getBmi}>
           <FormControl fullWidth>
-            <InputLabel>Enter your height in centimeters</InputLabel>
-            <Input type="number" value={height} onChange={this.handleHeight} />
+            <InputLabel>Enter your Weight in kilograms</InputLabel>
+            <Input type="number" value={weight} onChange={this.handleWeight} />
           </FormControl>
-        </div>
+          <div className="margin-top">
+            <FormControl fullWidth>
+              <InputLabel>Enter your height in centimeters</InputLabel>
+              <Input type="number" value={height} onChange={this.handleHeight} />
+            </FormControl>
+          </div>
 
-        <div className="margin-top">
-          <Button type="submit" fullWidth variant="contained" color="primary">
-            Get BMI
+          <div className="margin-top">
+            <Button type="submit" fullWidth variant="contained" color="primary">
+              Get BMI
           </Button>
-        </div>
+          </div>
         </form>
 
         {/* Result */}
-        <div style={{marginTop: "20px"}}>
+        <div style={{ marginTop: "20px" }}>
           <Typography color="inherit" variant="h2">Your BMI is: {bmi} </Typography>
-          <Typography color="inherit" variant="h3">State: <span style={{color: color}} > {state} </span> </Typography>
+          <Typography color="inherit" variant="h3">State: <span style={{ color: color }} > {state} </span> </Typography>
         </div>
       </div>
     );
