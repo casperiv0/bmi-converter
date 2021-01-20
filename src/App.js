c6ed6abff875a15be46ce7b7350d91ea70302b9c
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-01-20 17:03:51 +0100

    :tada: chore: update to React 17

--- src/App.js
+++ src/App.js
@@ -1,113 +1,99 @@
-import React, { Component } from 'react';
-import { InputLabel, FormControl, Input, Button, Typography, Link } from "@material-ui/core"
+import React, { useState } from "react";
+import { InputLabel, FormControl, Input, Button, Typography, Link } from "@material-ui/core";
 
+const BMICalculator = () => {
+  const [height, setHeight] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBmi] = useState(null);
+  const [color, setColor] = useState("");
+  const [state, setState] = useState("");
 
-class App extends Component {
-
-  constructor() {
-    super()
-
-    this.state = {
-      weight: 0,
-      height: 0,
-      bmi: 0,
-      state: "",
-      color: ""
-    }
-  }
-
-  handleHeight = (e) => {
-    this.setState({
-      height: e.target.value
-    });
-  };
-
-  handleWeight = (e) => {
-    this.setState({
-      weight: e.target.value
-    });
-  };
-
-  getBmi = (e) => {
+  function getBmi(e) {
     e.preventDefault();
-    const { weight, height } = this.state;
-    const bmi = (weight / ((height * height) / 10000)).toFixed(2)
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
 
+    if (height === "0" || weight === "0") {
+      setState("");
+      setBmi("Wow! I can't do that!");
+      return;
+    }
 
-
-    if (height === 0) {
-      return this.setState({
-        bmi: "Wow! I can't do that!"
-      });
-    };
-
-    this.setState({
-      bmi: bmi
-    })
+    setBmi(bmi);
 
     if (bmi <= 18.5) {
-      return this.setState({
-        state: "Underweight",
-        color: "orange"
-      });
-    };
-
-    if (bmi >= 18.5 && bmi <= 25) {
-      return this.setState({
-        state: "Normal",
-        color: "green"
-      });
+      setState("Underweight");
+      setColor("orange");
+      return;
     }
 
+    if (bmi >= 18.5 && bmi <= 25) {
+      setState("Normal");
+      setColor("green");
+      return;
+    }
 
     if (bmi >= 25 && bmi <= 30) {
-      return this.setState({
-        state: "Overweight",
-        color: "orange"
-      });
+      setState("Overweight");
+      setColor("orange");
+      return;
     }
 
     if (bmi > 31) {
-      return this.setState({
-        state: "Obesity",
-        color: "red"
-      });
-    };
-
+      setState("Obesity");
+      setColor("red");
+      return;
+    }
   }
 
-  render() {
-    const { weight, height, bmi, state, color } = this.state
-    return (
-      <div className="App" >
-        <Link target="_blank" variant="body2" href="https://github.com/Dev-CasperTheGhost/bmi-converter" >Source Code</Link>
-        <form  style={{marginTop: "20px"}} onSubmit={this.getBmi}>
+  return (
+    <div className="App">
+      <Link
+        target="_blank"
+        variant="body2"
+        href="https://github.com/Dev-CasperTheGhost/bmi-converter"
+      >
+        Source Code
+      </Link>
+      <form style={{ marginTop: "20px" }} onSubmit={getBmi}>
+        <FormControl fullWidth>
+          <InputLabel>Enter your Weight in kilograms</InputLabel>
+          <Input
+            required
+            type="number"
+            value={weight}
+            onChange={(e) => setWeight(e.currentTarget.value)}
+          />
+        </FormControl>
+        <div className="margin-top">
           <FormControl fullWidth>
-            <InputLabel>Enter your Weight in kilograms</InputLabel>
-            <Input type="number" value={weight} onChange={this.handleWeight} />
+            <InputLabel>Enter your height in centimeters</InputLabel>
+            <Input
+              required
+              type="number"
+              value={height}
+              onChange={(e) => setHeight(e.currentTarget.value)}
+            />
           </FormControl>
-          <div className="margin-top">
-            <FormControl fullWidth>
-              <InputLabel>Enter your height in centimeters</InputLabel>
-              <Input type="number" value={height} onChange={this.handleHeight} />
-            </FormControl>
-          </div>
-
-          <div className="margin-top">
-            <Button type="submit" fullWidth variant="contained" color="primary">
-              Get BMI
-          </Button>
-          </div>
-        </form>
-
-        {/* Result */}
-        <div style={{ marginTop: "20px" }}>
-          <Typography color="inherit" variant="h2">Your BMI is: {bmi} </Typography>
-          <Typography color="inherit" variant="h3">State: <span style={{ color: color }} > {state} </span> </Typography>
         </div>
+
+        <div className="margin-top">
+          <Button type="submit" fullWidth variant="contained" color="primary">
+            Get BMI
+          </Button>
+        </div>
+      </form>
+
+      {/* Result */}
+      <div style={{ marginTop: "20px" }}>
+        <Typography color="inherit" variant="h2">
+          Your BMI is: {bmi}
+        </Typography>
+        <Typography color="inherit" variant="h3">
+          State: <span style={{ color }}> {state} </span>
+        </Typography>
       </div>
-    );
-  };
+    </div>
+  );
 };
 
-export default App;
+export default BMICalculator;
